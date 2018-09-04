dea57be241f3372d2a044011782de92aac98d7b7
detailyang <detailyang@gmail.com>
2018-09-04 12:35:12 +0800

    path: touch more placeholder

--- fish/path.fish
+++ fish/path.fish
@@ -1,9 +1,6 @@
-set -x PATH ~/.bin $PATH
-set -x PATH ~/.cargo/bin $PATH
-set -x PATH ~/.go/bin $PATH
-
 set -x PATH ~/bin $PATH
 set -x PATH ~/cargo/bin $PATH
 set -x PATH ~/go/bin $PATH
 set -x PATH ~/python/bin $PATH
 set -x PATH ~/bash/bin $PATH
+set -x PATH ~/node/bin $PATH
