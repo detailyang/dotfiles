a847a10df22d7d07995820b5961b7ed2d080e1b1
detailyang <detailyang@gmail.com>
2018-09-04 12:16:03 +0800

    fish: support GOPROXY

--- fish/proxy.fish
+++ fish/proxy.fish
@@ -1,9 +1,11 @@
 function proxy -d "set proxy to socks5 1087"
 	export HTTPS_PROXY=http://127.0.0.1:1087
 	export HTTP_PROXY=http://127.0.0.1:1087
+	export GOPROXY=https://goproxy.io
 end
 
 function unproxy -d "disable proxy to socks5 1087"
-	export HTTPS_PROXY
-	export HTTP_PROXY
+	export HTTPS_PROXY=
+	export HTTP_PROXY=
+	export GOPROXY=
 end
