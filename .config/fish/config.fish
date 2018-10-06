aae71472a7e1b702d0173a23d5da9a1fa401b8c1
detailyang <detailyang@gmail.com>
2018-10-06 09:06:02 +0800

    config: bootstrap fisher when first use

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,10 @@
+if not functions -q fisher
+    echo "Installing fisher for the first time..." >&2
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fisher
+end
+
 for file in ~/.config/fish/conf.d/*.fish
 	source $file
 end
