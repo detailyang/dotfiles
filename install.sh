1506d7a0a2493c3f55337aa86f6697c22114a69b
detailyang <detailyang@gmail.com>
2018-11-03 10:36:11 +0800

    feature: add vscode

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ git pull --ff origin master
 rsync --exclude-from=./.exclude \
 	-avh --no-perms . ~
 
+rsync -avh --no-perms  vscode/ ~/Library/Application\ Support/Code/User/
+
 source ~/.bash_profile
 
 mkdir -p ~/art/{opensource,personal}
---
+++ vscode/backup.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+cd "$(dirname "${BASH_SOURCE}")"
+
+cp "$HOME/Library/Application Support/Code/User/keybindings.json" .
+cp "$HOME/Library/Application Support/Code/User/settings.json" .
+
+
