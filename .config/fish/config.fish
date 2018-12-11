b6c13f5aa0a86eaacca874f06b01e8f26f0f799a
detailyang <detailyang@gmail.com>
2018-12-11 21:24:16 +0800

    misc: chaos commit

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,14 +5,6 @@ if not functions -q fisher
     fisher
 end
 
-for file in ~/.config/fish/conf.d/*.fish
-	source $file
-end
-
-for file in ~/.fish/*.fish
-	source $file
-end
-
 for file in ~/fish/*.fish
 	source $file
 end
--- install.sh
+++ install.sh
@@ -12,3 +12,8 @@ rsync -avh --no-perms  vscode/ ~/Library/Application\ Support/Code/User/
 source ~/.bash_profile
 
 mkdir -p ~/art/{opensource,personal}
+
+if [[ $(uname) == "Darwin" ]]; then
+    echo "Disable OSX avoiding the vscode PressAndHoldEnabled"
+    defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+fi
