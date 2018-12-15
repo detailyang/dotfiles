ef70c3f4a3c3190c156c438009ccf5b004c1461d
detailyang <detailyang@gmail.com>
2018-12-15 19:17:53 +0800

    feature: proxy function support local vm linux

--- fish/proxy.fish
+++ fish/proxy.fish
@@ -1,6 +1,15 @@
 function proxy -d "set proxy to socks5 1087"
-	export HTTPS_PROXY=http://127.0.0.1:1087
-	export HTTP_PROXY=http://127.0.0.1:1087
+	switch (uname)
+	case Linux
+		export HTTPS_PROXY=http://192.168.33.1:1087
+		export HTTP_PROXY=http://192.168.33.1:1087
+	case Darwin
+		export HTTPS_PROXY=http://127.0.0.1:1087
+		export HTTP_PROXY=http://127.0.0.1:1087
+	case '*'
+		export HTTPS_PROXY=http://127.0.0.1:1087
+		export HTTP_PROXY=http://127.0.0.1:1087
+	end
 	export GOPROXY=https://goproxy.io
 end
 
