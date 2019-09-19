77d47480f2808107a6ce53a95853650fe9c67617
detailyang <detailyang@gmail.com>
2019-09-19 13:25:56 +0800

    change:remove vscode because of sync plugin
    Signed-off-by: detailyang <detailyang@gmail.com>

--- vscode/backup.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-cd "$(dirname "${BASH_SOURCE}")"
-
-cp "$HOME/Library/Application Support/Code/User/keybindings.json" .
-cp "$HOME/Library/Application Support/Code/User/settings.json" .
-
-
