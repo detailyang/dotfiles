acd4d16f32cd53d345c95bd9edf5ecfe89ac00dd
detailyang <detailyang@gmail.com>
2019-09-22 22:27:36 +0800

    chore:remove vscode
    Signed-off-by: detailyang <detailyang@gmail.com>

--- install.sh
+++ install.sh
@@ -12,8 +12,3 @@ rsync -avh --no-perms  vscode/ ~/Library/Application\ Support/Code/User/
 source ~/.bash_profile
 
 mkdir -p ~/art/{opensource,personal}
-
-if [[ $(uname) == "Darwin" ]]; then
-    echo "Disable OSX avoiding the vscode PressAndHoldEnabled"
-    defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
-fi
