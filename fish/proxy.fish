9ec2aed2d26a89636d40f6e093fceb9cf1280475
detailyang <detailyang@gmail.com>
2019-10-31 12:23:53 +0800

    feat:fish:add NOPROXY
    Signed-off-by: detailyang <detailyang@gmail.com>

--- fish/proxy.fish
+++ fish/proxy.fish
@@ -11,10 +11,12 @@ function proxy -d "set proxy to socks5 1087"
 	export HTTPS_PROXY=http://$ip:1087
 	export HTTP_PROXY=http://$ip:1087
 	export GOPROXY=https://goproxy.cn
+    export NOPROXY="127.0.0.1,192.168.64.11"
 end
 
 function unproxy -d "disable proxy to socks5 1087"
 	export HTTPS_PROXY=
 	export HTTP_PROXY=
 	export GOPROXY=
+    export NOPROXY=
 end
