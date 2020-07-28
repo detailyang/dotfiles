ba944f7be5f97e4b6de2c3c2b2a06d50d5f30c84
detailyang <detailyang@gmail.com>
2020-07-28 21:59:04 +0800

    chore:add brew
    Signed-off-by: detailyang <detailyang@gmail.com>

--- fish/brew.fish
+++ fish/brew.fish
@@ -1,1 +1,8 @@
-export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles/
+
+cd /usr/local/Homebrew
+git remote set-url origin git://mirrors.ustc.edu.cn/brew.git
+
+cd /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core
+git remote set-url origin git://mirrors.ustc.edu.cn/brew.git
+
+export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
