373c615a8a906971b1fcdd32b0ecabed6c08dad9
detailyang <detailyang@gmail.com>
2020-08-18 11:22:43 +0800

    feat:check rust app in install.sh
    Signed-off-by: detailyang <detailyang@gmail.com>

--- install.sh
+++ install.sh
@@ -2,11 +2,32 @@
 
 cd "$(dirname "${BASH_SOURCE}")"
 
-git pull --ff origin master
+function install_rust_app() {
+    readonly apps=(fd bat hyperfine hexyl pastel)
+    for app in "${apps[@]}"; do
+        if ! command -v $app &> /dev/null; then
+            brew install $app
+        fi
+    done
+}
 
-rsync --exclude-from=./.exclude \
-	-avh --no-perms . ~
 
-source ~/.bash_profile
+function main() {
+    echo "Pulling the lastest changes"
+    git pull --ff origin master
 
-mkdir -p ~/art/{opensource,personal}
+    echo "Rsyncing to target"
+    rsync --exclude-from=./.exclude \
+        -avh --no-perms . ~
+
+    echo "Sourcing bash profile"
+    source ~/.bash_profile
+
+    echo "Prepare directory"
+    mkdir -p ~/art/{opensource,personal}
+
+    echo "Install rust app"
+    install_rust_app;
+}
+
+main
