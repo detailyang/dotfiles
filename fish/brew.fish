0c86a1bb53747b5b7891504349374d7f427fd206
detailyang <detailyang@gmail.com>
2020-08-20 14:28:07 +0800

    bugfix:fish:use cd ~
    Signed-off-by: detailyang <detailyang@gmail.com>

--- fish/brew.fish
+++ fish/brew.fish
@@ -1,11 +1,9 @@
-pushd
 cd /usr/local/Homebrew
 git remote set-url origin git://mirrors.ustc.edu.cn/brew.git
-popd
 
-pushd
 cd /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core
 git remote set-url origin git://mirrors.ustc.edu.cn/brew.git
-popd
+
+cd ~
 
 export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
