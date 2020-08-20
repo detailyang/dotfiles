fbb0975bf34c266f2c6fa8253a0183600101c784
detailyang <detailyang@gmail.com>
2020-08-20 14:26:51 +0800

    feat:fish:save working directory
    Signed-off-by: detailyang <detailyang@gmail.com>

--- fish/brew.fish
+++ fish/brew.fish
@@ -1,8 +1,11 @@
-
+pushd
 cd /usr/local/Homebrew
 git remote set-url origin git://mirrors.ustc.edu.cn/brew.git
+popd
 
+pushd
 cd /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core
 git remote set-url origin git://mirrors.ustc.edu.cn/brew.git
+popd
 
 export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
