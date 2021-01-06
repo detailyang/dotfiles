44361e54fa62f7f945c727b849b92e03349e3694
detailyang <detailyang@gmail.com>
2021-01-06 14:13:24 +0800

    chore:remove fisher installer
    Signed-off-by: detailyang <detailyang@gmail.com>

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,3 @@
-if not functions -q fisher
-    echo "Installing fisher for the first time..." >&2
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fisher
-end
-
 for file in ~/fish/*.fish
 	source $file
 end
