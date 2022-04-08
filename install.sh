1b36971ef23542a0f43886aedaff7839d69bad54
detailyang <detailyang@gmail.com>
2022-04-08 17:34:14 +0800

    feat:add mac apps
    Signed-off-by: detailyang <detailyang@gmail.com>

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cd "$(dirname "${BASH_SOURCE}")"
 function install_mac_app() {
     # defaults write -g applepressandholdenabled -bool false
 
-    readonly apps=(amphetamine bartender enpass snippetslab eudic reeder dash karabiner)
+    readonly apps=(amphetamine bartender enpass snippetslab eudic reeder dash karabiner itsycal paper)
 
     for app in "${apps[@]}"; do
         echo "try to install $app"
