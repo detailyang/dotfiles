76adbe3464d1e10853abfc217d60f63b74913a2a
detailyang <detailyang@gmail.com>
2022-04-08 17:12:05 +0800

    feat:hammerspoon:add hotkey lua module
    Signed-off-by: detailyang <detailyang@gmail.com>

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -2,6 +2,7 @@ require "modules.reload"
 require "modules.volume"
 require "modules.window"
 require "modules.gitautopush"
+require "modules.hotkey"
 
 hs.hotkey.bind({'cmd', 'shift'}, 'h', function() 
 	hs.notify.new({title="Hammerspoon launch", informativeText="Boss, at your service"}):send()
