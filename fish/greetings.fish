65b99d8503a05bf9ac38aa87f06a7770f21ee923
detailyang <detailyang@gmail.com>
2022-04-14 15:21:17 +0800

    chore:tewak greetings
    Signed-off-by: detailyang <detailyang@gmail.com>

--- fish/greetings.fish
+++ fish/greetings.fish
@@ -2,7 +2,7 @@ functions -c fish_greeting _old_fish_greeting
 function fish_greeting
     #_old_fish_greeting
     if type -q cowsay and type -q fortune and type -q lolcat
-        fortune -s | cowsay | lolcat -t
+        fortune -e art | cowsay | lolcat -t
     end
 end
 
