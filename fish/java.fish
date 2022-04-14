06c500bd4f74f54d2dfafa75166f6128804facb6
detailyang <detailyang@gmail.com>
2022-04-14 15:31:47 +0800

    chore:fish:tweak java.fish to avoid greeting
    Signed-off-by: detailyang <detailyang@gmail.com>

--- fish/java.fish
+++ fish/java.fish
@@ -1,2 +1,2 @@
 set jdk (command ls /Library/Java/JavaVirtualMachines | head -n 1)
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/$jdk/Contents/Home
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/$jdk/Contents/Home"
