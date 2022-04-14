e2c4dac57bc80d28ce48cc50c2803c5808783119
detailyang <detailyang@gmail.com>
2022-04-14 15:43:56 +0800

    chore:fish:tweak greetings
    Signed-off-by: detailyang <detailyang@gmail.com>

--- fish/greetings.fish
+++ fish/greetings.fish
@@ -2,7 +2,7 @@ functions -c fish_greeting _old_fish_greeting
 function fish_greeting
     #_old_fish_greeting
     if type -q cowsay and type -q fortune and type -q lolcat
-        fortune -s -e art | cowsay | lolcat -t
+        fortune -s -e art | cowsay -n | lolcat -t
     end
 end
 
