9fe57319d9048ffe31a9807151974cac70640082
detailyang <detailyang@gmail.com>
2022-04-14 18:06:01 +0800

    chore:fish:tweak osx compiler option
    Signed-off-by: detailyang <detailyang@gmail.com>

--- fish/osx.fish
+++ fish/osx.fish
@@ -1,5 +1,10 @@
 function osx
+    export LD_LIBRARY_PATH="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib:$LD_LIBRARY_PATH"
     export LIBRARY_PATH="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib:$LIBRARY_PATH"
+    export C_INCLUDE_PATH="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include:$C_INCLUDE_PATH"
+    export CPLUS_INCLUDE_PATH="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include:$CPLUS_INCLUDE_PATH"
+    export CFLAGS="-I /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include"
+    export LDFLAGS="-L /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib"
     export PATH="/Library/Developer/CommandLineTools/usr/bin/:$PATH"
 end
 
