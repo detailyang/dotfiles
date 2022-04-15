d0e634d67f369a328574e3fd554588f69f6eae9e
detailyang <detailyang@gmail.com>
2022-04-15 11:00:20 +0800

    feat:fish:redirect stdout
    Signed-off-by: detailyang <detailyang@gmail.com>

--- fish/osx.fish
+++ fish/osx.fish
@@ -1,11 +1,11 @@
 function osx
-    export LD_LIBRARY_PATH="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib:$LD_LIBRARY_PATH"
-    export LIBRARY_PATH="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib:$LIBRARY_PATH"
-    export C_INCLUDE_PATH="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include:$C_INCLUDE_PATH"
-    export CPLUS_INCLUDE_PATH="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include:$CPLUS_INCLUDE_PATH"
-    export CFLAGS="-I /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include"
-    export LDFLAGS="-L /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib"
-    export PATH="/Library/Developer/CommandLineTools/usr/bin/:$PATH"
+    export LD_LIBRARY_PATH="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib:$LD_LIBRARY_PATH" &> /dev/null
+    export LIBRARY_PATH="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib:$LIBRARY_PATH" &> /dev/null
+    export C_INCLUDE_PATH="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include:$C_INCLUDE_PATH" &> /dev/null
+    export CPLUS_INCLUDE_PATH="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include:$CPLUS_INCLUDE_PATH" &> /dev/null
+    export CFLAGS="-I /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include" &> /dev/null
+    export LDFLAGS="-L /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib" &> /dev/null
+    export PATH="/Library/Developer/CommandLineTools/usr/bin/:$PATH" &> /dev/null
 end
 
 defaults delete -g ApplePressAndHoldEnabled 2>&1 &> /dev/null # If necessary, reset global default
