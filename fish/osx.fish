5f91960fc751ca62eb99c90e22941701df3832f3
detailyang <detailyang@gmail.com>
2022-04-15 15:27:48 +0800

    chore:fish:tweak osx function
    Signed-off-by: detailyang <detailyang@gmail.com>

--- fish/osx.fish
+++ fish/osx.fish
@@ -1,11 +1,12 @@
 function osx
+    export FRAMEWORK_SEARCH_PATHS="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/System/Library/Frameworks/" &> /dev/null
     export LD_LIBRARY_PATH="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib:$LD_LIBRARY_PATH" &> /dev/null
     export LIBRARY_PATH="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib:$LIBRARY_PATH" &> /dev/null
     export C_INCLUDE_PATH="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include:$C_INCLUDE_PATH" &> /dev/null
     export CPLUS_INCLUDE_PATH="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include:$CPLUS_INCLUDE_PATH" &> /dev/null
     export CFLAGS="-I /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include" &> /dev/null
     export LDFLAGS="-L /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib" &> /dev/null
-    export PATH="/Library/Developer/CommandLineTools/usr/bin/:$PATH" &> /dev/null
+    export PATH="/usr/bin:/Library/Developer/CommandLineTools/usr/bin/:$PATH" &> /dev/null
 end
 
 defaults delete -g ApplePressAndHoldEnabled 2>&1 &> /dev/null # If necessary, reset global default
