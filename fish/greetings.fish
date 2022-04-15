8912bc775de9c505c753115b1739dcd873a215eb
detailyang <detailyang@gmail.com>
2022-04-15 19:57:53 +0800

    feat:fish:tweak greeting
    Signed-off-by: detailyang <detailyang@gmail.com>

--- fish/greetings.fish
+++ fish/greetings.fish
@@ -2,7 +2,7 @@ functions -c fish_greeting _old_fish_greeting
 function fish_greeting
     #_old_fish_greeting
     if type -q fortune and type -q lolcat
-        fortune -s -e art | lolcat -t
+        fortune -s -n 160 | lolcat -t
     end
 end
 
