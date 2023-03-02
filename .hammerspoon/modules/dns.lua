9ecb51fc1b2d667d62ac4ddfedd97c610bd4c7ce
detailyang <detailyang@gmail.com>
2023-03-02 14:37:43 +0800

    chore:hammerspoon:disable dns

--- .hammerspoon/modules/dns.lua
+++ .hammerspoon/modules/dns.lua
@@ -1,8 +1,22 @@
 
 function changedns()
-    local cmd = "networksetup -setdnsservers Wi-Fi 223.5.5.5 223.6.6.6"
-    print("Change dns resolver: " .. cmd)
-    shell(cmd)
+    local _, output,_ = shell("networksetup -getdnsservers Wi-Fi")
+    local servers = splitlines(output)
+
+    for _, server in ipairs({"223.5.5.5","223.6.6.6"}) do
+        if indexOf(servers, server) == nil then
+            servers[#servers+1] = server
+        end
+    end
+
+    local setcmd = "networksetup -setdnsservers Wi-Fi " .. table.concat(servers, " ")
+    print("Change dns resolver: " .. setcmd)
+    shell(setcmd)
 end
 
 hs.timer.doEvery(600, changedns)
+
+
+hs.hotkey.bind({'cmd', 'shift'}, 'd', function() 
+    changedns()
+end)
--- .hammerspoon/modules/helper.lua
+++ .hammerspoon/modules/helper.lua
@@ -1,3 +1,34 @@
 function shell(cmd)
-    result = hs.osascript.applescript(string.format('do shell script "%s"', cmd))
+    local result, output, desp = hs.osascript.applescript(string.format('do shell script "%s"', cmd))
+    return result, output, desp
+end
+
+function splitlines(input)
+    local lines = {}
+    for s in input:gmatch("[^\r\n]+") do
+        table.insert(lines, s)
+    end
+    return lines
+end
+
+function dump(o)
+    if type(o) == 'table' then
+       local s = '{ '
+       for k,v in pairs(o) do
+          if type(k) ~= 'number' then k = '"'..k..'"' end
+          s = s .. '['..k..'] = ' .. dump(v) .. ','
+       end
+       return s .. '} '
+    else
+       return tostring(o)
+    end
+ end
+
+ function indexOf(array, value)
+    for i, v in ipairs(array) do
+        if v == value then
+            return i
+        end
+    end
+    return nil
 end
