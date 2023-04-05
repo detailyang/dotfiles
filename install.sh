033d59c633b20a2814535d4be6147fc2f6242f6b
detailyang <detailyang@gmail.com>
2023-04-06 00:09:51 +0800

    chore:tweak apps
    Signed-off-by: detailyang <detailyang@gmail.com>

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cd "$(dirname "${BASH_SOURCE}")"
 function install_mac_app() {
     # defaults write -g applepressandholdenabled -bool false
 
-    readonly apps=(shottr Hovrly NetNewsWire amphetamine bartender enpass snippetslab eudic reeder dash karabiner itsycal paper hammersppon)
+    readonly apps=(shottr Hovrly NetNewsWire amphetamine bartender enpass snippetslab eudic reeder dash karabiner itsycal paper hammersppon lulu)
 
     for app in "${apps[@]}"; do
         echo "try to install $app"
