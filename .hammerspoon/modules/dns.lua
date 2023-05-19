a7a31b745ee0fd030a8fce5fbe0d8ac7456649b9
detailyang <detailyang@gmail.com>
2023-05-19 10:13:06 +0800

    hammerspoon:tweak dns to 127.0.0.1
    Signed-off-by: detailyang <detailyang@gmail.com>

--- .hammerspoon/modules/dns.lua
+++ .hammerspoon/modules/dns.lua
@@ -1,15 +1,15 @@
 
 function changedns()
-    local _, output,_ = shell("networksetup -getdnsservers Wi-Fi")
-    local servers = splitlines(output)
+    -- local _, output,_ = shell("networksetup -getdnsservers Wi-Fi")
+    -- local servers = splitlines(output)
 
-    for _, server in ipairs({"127.0.0.1"}) do
-        if indexOf(servers, server) == nil then
-            servers[#servers+1] = server
-        end
-    end
+    -- for _, server in ipairs({"127.0.0.1"}) do
+    --     if indexOf(servers, server) == nil then
+    --         servers[#servers+1] = server
+    --     end
+    -- end
 
-    local setcmd = "networksetup -setdnsservers Wi-Fi " .. table.concat(servers, " ")
+    local setcmd = "networksetup -setdnsservers Wi-Fi 127.0.0.1" 
     print("Change dns resolver: " .. setcmd)
     shell(setcmd)
 end
