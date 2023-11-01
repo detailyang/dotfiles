a73bb5bbd2c1c80c4bd4eb284c43ff2b72fd3e58
detailyang <detailyang@gmail.com>
2023-11-01 15:14:08 +0800

    chore:wezterm:tewak fonts

--- .config/wezterm/config/fonts.lua
+++ .config/wezterm/config/fonts.lua
@@ -4,7 +4,7 @@ local platform = require('utils.platform')
 local font_size = platform().is_mac and 14 or 12
 
 return {
-   font = wezterm.font('JetBrains Mono', { weight = 'Bold', italic = true }),
+   font = wezterm.font('JetBrains Mono', { weight = 'Bold', italic = false }),
    font_size = font_size,
    warn_about_missing_glyphs = false,
    --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
