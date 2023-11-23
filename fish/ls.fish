b5413aef439bfdc4531262cbd5e0fb1fa3bcb5b9
detailyang <detailyang@gmail.com>
2023-11-24 01:03:00 +0800

    Update ls.fish

--- fish/ls.fish
+++ fish/ls.fish
@@ -1,7 +1,9 @@
 function ls -d "list fies"
     if type -q exa
         command exa $argv
-    else 
+    else if type -q eza
+        command eza $argv
+    else
         command ls $argv
     end
 end
