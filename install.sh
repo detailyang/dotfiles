0571e8fe92ad7bcf6c9ba1539a397848952dd18e
detailyang <detailyang@gmail.com>
2025-06-17 12:33:27 +0800

    chore:tweak key repeat
    Signed-off-by: detailyang <detailyang@gmail.com>

--- install.sh
+++ install.sh
@@ -65,9 +65,9 @@ function prepare_init_darwin() {
 
     defaults write -g AppleFontSmoothing -int 1
 
-    defaults write -g KeyRepeat -int 1
+    defaults write -g KeyRepeat -int 3
 
-    defaults write -g InitialKeyRepeat -int 10
+    defaults write -g InitialKeyRepeat -int 3
 
     killall Finder;
 }
