2e70055391c7bc4edc341f34d8c4702ca60cb475
detailyang <detailyang@gmail.com>
2026-05-12 23:45:52 +0800

    Delete darwin/bark.popclipext directory

--- darwin/bark.popclipext/bark.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-URL=$(cat ~/.bark)
-
-if [[ -z "$POPCLIP_URLENCODED_TEXT" ]]; then
-    POPCLIP_URLENCODED_TEXT=$(perl -MURI::Escape -e "print uri_escape('$1');")
-fi
-
-curl -v $URL$POPCLIP_URLENCODED_TEXT?copy=$POPCLIP_URLENCODED_TEXT
